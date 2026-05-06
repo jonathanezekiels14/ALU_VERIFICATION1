c94e0bc71eec22f007ec5d7bdde6fe004c07dffc
Jonathan Ezekiels <jonathanezekiels14@gmail.com>
2026-05-06 14:17:27 +0530

    Completed ALU, with multiple bugs corrected

--- alu_design.v
+++ alu_design.v
@@ -10,17 +10,20 @@ module alu_design #(parameter WIDTH=8)(
 	reg [1:0] count;
 	reg [WIDTH-1:0] temp_mul,tempA, tempB;
 	wire [WIDTH-1:0] sum_w,diff_w;
+	reg [WIDTH-1:0] OPA_w,OPB_w;
 
 
-	assign sum_w=OPA+OPB;
-	assign diff_w=OPA-OPB;	
+	assign sum_w=OPA_w+OPB_w;
+	assign diff_w=OPA_w-OPB_w;	
 	always @(posedge CLK) begin
 		if (MODE == 1 && (CMD == 9 || CMD == 10)) begin
 			if (count < 2) 
 				count <= count + 1;
 			else
-				count <= 0;
+				count <= 1;
 		end
+      	else
+          		count<=0;
 	end
 	always @(posedge CLK or posedge RST) begin    //Asynchronous Active High RESET
 		if(RST) begin
@@ -32,9 +35,15 @@ module alu_design #(parameter WIDTH=8)(
 			E<=0;
 			OFLOW<=0;
 			count<=0;
+            OPA_w<=0;
+            OPB_w<=0;
 		end
-		else begin
+      	else begin
 			if(CE) begin
+                // Latch inputs regardless of MODE so both Logic and Arithmetic get them
+                OPA_w<=OPA;
+                OPB_w<=OPB;
+
 				if(MODE) begin       //Mode - 1: Arithmetic Operation | Mode - 0: Logical Operation
 					RES<=0;
                         		COUT<=0;
@@ -45,9 +54,9 @@ module alu_design #(parameter WIDTH=8)(
                         		OFLOW<=0;
 					case (CMD)
 						0: case( INP_VALID) // CMD - 0 : ADD
-							3:begin     // Perform OPA+OPB only if INP_VALID = 3
-								RES<= OPA+OPB;
-								COUT<=(OPA + OPB) > ((1<<WIDTH)-1);
+							3:begin     // Perform OPA_w+OPB_w only if INP_VALID = 3
+								RES<= OPA_w+OPB_w;
+								COUT<=(OPA_w + OPB_w) > ((1<<WIDTH)-1);
 							end
 							default: begin     // Else ERR=1 , RES = 0
 								RES<=0;
@@ -55,9 +64,9 @@ module alu_design #(parameter WIDTH=8)(
 							end                              
 						endcase
 						1: case( INP_VALID)  // CMD -1 : SUB
-							3: begin  // Perform OPA-OPB only if INP_VALID = 3
-								OFLOW<=(OPA<OPB)?1:0;
-								RES<=OPA-OPB;
+							3: begin  // Perform OPA_w-OPB_w only if INP_VALID = 3
+								OFLOW<=(OPA_w<OPB_w)?1:0;
+								RES<=OPA_w-OPB_w;
 							end
 							default: begin // Else ERR=1 , RES = 0
 								RES<=0;
@@ -66,8 +75,8 @@ module alu_design #(parameter WIDTH=8)(
 						endcase
 						2: case( INP_VALID)  // CMD - 2 : ADD_CIN
 							3: begin
-								RES<=OPA+OPB+CIN;
-								COUT <= (OPA + OPB + CIN) > ((1<<WIDTH)-1);
+								RES<=OPA_w+OPB_w+CIN;
+								COUT <= (OPA_w + OPB_w + CIN) > ((1<<WIDTH)-1);
 							end
 							default: begin
 								RES<=0;
@@ -76,8 +85,8 @@ module alu_design #(parameter WIDTH=8)(
 						endcase
 						3: case(INP_VALID)  // CMD - 3 : SUB_CIN
 							3: begin
-								OFLOW<=(OPA<(OPB+CIN))?1:0;
-								RES<=OPA-OPB-CIN;
+								OFLOW<=(OPA_w<(OPB_w+CIN))?1:0;
+								RES<=OPA_w-OPB_w-CIN;
 							end
 							default: begin
 								RES<=0;
@@ -85,32 +94,32 @@ module alu_design #(parameter WIDTH=8)(
 							end
 						endcase
 						4: case (INP_VALID)  // CMD - 4 : INC_A 
-							1: RES<=OPA+1;
-							3: RES<=OPA+1;
+							1: RES<=OPA_w+1;
+							3: RES<=OPA_w+1;
 							default: begin
 								RES<=0;
 								ERR<=1;
 							end
 						endcase
 						5: case (INP_VALID)  // CMD - 5 : DEC_A 
-							1: RES<=OPA-1;
-							3: RES<=OPA-1;
+							1: RES<=OPA_w-1;
+							3: RES<=OPA_w-1;
 							default: begin
 								RES<=0;
 								ERR<=1;
 							end
 						endcase              
 						6: case(INP_VALID) // CMD - 6 : INC_B
-							2: RES<=OPB+1;
-							3: RES<=OPB+1;
+							2: RES<=OPB_w+1;
+							3: RES<=OPB_w+1;
 							default: begin
 								RES<=0;
 								ERR<=1;
 							end
 						endcase
 						7:case(INP_VALID) // CMD - 7 : DEC_B
-							2: RES<=OPB-1;
-							3: RES<=OPB-1;
+							2: RES<=OPB_w-1;
+							3: RES<=OPB_w-1;
 							default: begin
 								RES<=0;
 								ERR<=1;
@@ -118,9 +127,9 @@ module alu_design #(parameter WIDTH=8)(
 						endcase
 						8: case(INP_VALID) // CMD  - 8: CMP
 							3: begin
-								if(OPA==OPB) 
+								if(OPA_w==OPB_w) 
 									{E,G,L}=3'b100;
-								else if (OPA>OPB)
+								else if (OPA_w>OPB_w)
 									{E,G,L}=3'b010;
 								else 
 									{E,G,L}=3'b001;
@@ -130,17 +139,15 @@ module alu_design #(parameter WIDTH=8)(
 						9: case (INP_VALID) //CMD - 9: MULTIPLICATION w INC_A and INC_B
 			 				3: begin
 								case(count)
-									0: begin
-										tempA<=OPA+1;
-										tempB<=OPB+1;
-									end
 									1: begin
-										temp_mul<=tempA*tempB;
+										tempA<=OPA_w+1;
+										tempB<=OPB_w+1;
+                                      	RES <= {2*WIDTH{1'bx}};
 									end
 									2: begin
-										RES<=temp_mul;
+										RES<=tempA*tempB;
 									end
-									default: RES<=0;
+									default: RES<=RES;
 								endcase
 							end
 							default: begin
@@ -151,17 +158,14 @@ module alu_design #(parameter WIDTH=8)(
 						10: case (INP_VALID) //CMD - 10: MULTIPLICATION w SHIL_A
                                                         3: begin
                                                                 case(count)
-                                                                        0: begin
-                                                                                tempA<=OPA;
-                                                                                tempB<=OPB;
-                                                                        end
                                                                         1: begin
-                                                                                temp_mul<=(tempA<<1)*tempB;
+                                                                                tempA<=OPA_w<<1;
+                                                                          		RES<='bx;
                                                                         end
                                                                         2: begin
-                                                                                RES<=temp_mul;
-									end
-                                                                        default: RES<=0;
+                                                                                RES<=tempA*OPB_w;
+                                                                        end
+                                                                        default: RES<=RES;
                                                                 endcase
 							end
                                                         default: begin
@@ -171,11 +175,11 @@ module alu_design #(parameter WIDTH=8)(
 						endcase
 						11: case(INP_VALID) // CMD - 11: SIGNED ADDITION
 							3: begin
-								RES<=$signed(OPA) + $signed(OPB);
-								OFLOW<=(OPA[WIDTH-1] == OPB[WIDTH-1]) && (OPA[WIDTH-1] != sum_w[WIDTH-1]);
-								G<=($signed(OPA)>$signed(OPB));
-								L<=($signed(OPA)<$signed(OPB));
-								E<=(OPA==OPB);		
+								RES<=$signed(OPA_w) + $signed(OPB_w);
+								OFLOW<=(OPA_w[WIDTH-1] == OPB_w[WIDTH-1]) && (OPA_w[WIDTH-1] != sum_w[WIDTH-1]);
+								G<=($signed(OPA_w)>$signed(OPB_w));
+								L<=($signed(OPA_w)<$signed(OPB_w));
+								E<=(OPA_w==OPB_w);		
 							end
                                                         default: begin
                                                                         RES<=0;
@@ -184,11 +188,11 @@ module alu_design #(parameter WIDTH=8)(
 						endcase
 						12: case(INP_VALID)	// CMD - 12: SIGNED SUBTRACTION
 							3: begin
-								RES<= OPA + ( ~OPB + 1);
-								OFLOW<=(OPA[WIDTH-1] ^ OPB[WIDTH-1]) & (diff_w[WIDTH-1] ^ OPA[WIDTH-1]);
-								G<=($signed(OPA)>$signed(OPB));
-								L<=($signed(OPA)<$signed(OPB));
-								E<=(OPA==OPB);		
+								RES<= OPA_w + ( ~OPB_w + 1);
+								OFLOW<=(OPA_w[WIDTH-1] ^ OPB_w[WIDTH-1]) & (diff_w[WIDTH-1] ^ OPA_w[WIDTH-1]);
+								G<=($signed(OPA_w)>$signed(OPB_w));
+								L<=($signed(OPA_w)<$signed(OPB_w));
+								E<=(OPA_w==OPB_w);		
 							end
                                                         default: begin
                                                                         RES<=0;
@@ -209,90 +213,90 @@ module alu_design #(parameter WIDTH=8)(
                         		OFLOW<=0;
 					case (CMD)
 						0: case( INP_VALID) // CMD - 0 : BITWISE AND
-							3: RES<= OPA & OPB;
+							3: RES<= OPA_w & OPB_w;
 							default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						1: case(INP_VALID) // CMD - 1 : BITWISE NAND
-							3: RES<= ~(OPA & OPB);
+							3: RES<= ~(OPA_w & OPB_w);
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						2: case(INP_VALID) // CMD - 2 : BITWISE OR
-                                                        3: RES<= OPA | OPB;
+                                                        3: RES<= OPA_w | OPB_w;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						3: case(INP_VALID) // CMD - 3 : BITWISE NOR
-                                                        3: RES<= ~(OPA | OPB);
+                                                        3: RES<= ~(OPA_w | OPB_w);
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						4: case(INP_VALID) // CMD - 4 : BITWISE XOR
-                                                        3: RES<= OPA ^ OPB;
+                                                        3: RES<= OPA_w ^ OPB_w;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						5: case(INP_VALID) // CMD - 5 : BITWISE XNOR
-                                                        3: RES<= ~(OPA ^ OPB);
+                                                        3: RES<= ~(OPA_w ^ OPB_w);
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						6: case(INP_VALID) // CMD - 6 : NOT_A
-							1: RES <= ~OPA;
-							3: RES <= ~OPA;
+							1: RES <= ~OPA_w;
+							3: RES <= ~OPA_w;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						7: case(INP_VALID) // CMD - 7 : NOT_B
-							2: RES <= ~OPB;
-							3: RES <= ~OPB;
+							2: RES <= ~OPB_w;
+							3: RES <= ~OPB_w;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						8: case(INP_VALID) // CMD - 8 : SHR1_A
-							1: RES[WIDTH-1:0] <= OPA>>1;
-							3: RES[WIDTH-1:0] <= OPA>>1;
+							1: RES[WIDTH-1:0] <= OPA_w>>1;
+							3: RES[WIDTH-1:0] <= OPA_w>>1;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						9: case(INP_VALID) // CMD - 9 : SHL1_A
-							1: RES [WIDTH-1:0] <= OPA<<1;
-							3: RES [WIDTH-1:0]<= OPA<<1;
+							1: RES [WIDTH-1:0] <= OPA_w<<1;
+							3: RES [WIDTH-1:0]<= OPA_w<<1;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						10: case(INP_VALID) // CMD - 10 : SHR1_B
-							2: RES[WIDTH-1:0] <= OPB>>1;
-							3: RES[WIDTH-1:0] <= OPB>>1;
+							2: RES[WIDTH-1:0] <= OPB_w>>1;
+							3: RES[WIDTH-1:0] <= OPB_w>>1;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
                                                         end
 						endcase
 						11: case(INP_VALID) // CMD - 11 : SHL1_B
-							2: RES [WIDTH-1:0] <= OPB<<1;
-							3: RES [WIDTH-1:0]<= OPB<<1;
+							2: RES [WIDTH-1:0] <= OPB_w<<1;
+							3: RES [WIDTH-1:0]<= OPB_w<<1;
                                                         default: begin
                                                                         RES<=0;
                                                                         ERR<=1;
@@ -300,12 +304,12 @@ module alu_design #(parameter WIDTH=8)(
 						endcase
 						12: case(INP_VALID) // CMD - 12 : ROL_A_B
 							3: begin
-								if (|(OPB[(2*WIDTH)-1 : WIDTH])) begin
+								if (|(OPB_w[(2*WIDTH)-1 : WIDTH])) begin
             								ERR <= 1'b1;
 								end
 								else begin
 									ERR <= 1'b0;
-									RES[WIDTH-1:0] <= (OPA << (OPB % WIDTH)) | (OPA >> (WIDTH - (OPB % WIDTH)));
+									RES[WIDTH-1:0] <= (OPA_w << (OPB_w % WIDTH)) | (OPA_w >> (WIDTH - (OPB_w % WIDTH)));
 								        RES[(2*WIDTH)-1 : WIDTH] <= 0;
 								end
 							end
@@ -316,12 +320,12 @@ module alu_design #(parameter WIDTH=8)(
 						endcase
 						13: case(INP_VALID) // CMD - 13 : ROR_A_B
 							3: begin
-								if (|(OPB[(2*WIDTH)-1 : WIDTH])) begin
+								if (|(OPB_w[(2*WIDTH)-1 : WIDTH])) begin
             								ERR <= 1'b1;
 								end
 								else begin
 									ERR <= 1'b0;
-									RES[WIDTH-1:0] <= (OPA >> (OPB % WIDTH)) | (OPA << (WIDTH - (OPB % WIDTH)));
+									RES[WIDTH-1:0] <= (OPA_w >> (OPB_w % WIDTH)) | (OPA_w << (WIDTH - (OPB_w % WIDTH)));
 								        RES[(2*WIDTH)-1 : WIDTH] <= 0;
 								end
 							end
@@ -349,4 +353,3 @@ module alu_design #(parameter WIDTH=8)(
         	end
 	end
 endmodule
-
